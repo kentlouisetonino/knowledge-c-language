972109223e1cdd927e7babbae927184a1f4f59cd
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-12 21:36:18 +0800

    Handle the issue of input buffer.

--- 01-sololearn-introduction-to-C/01-basic-concepts/04-input-output.md
+++ 01-sololearn-introduction-to-C/01-basic-concepts/04-input-output.md
@@ -1,7 +1,20 @@
 ### Input
 #
 
-```
+> - Refer to `code-04.c` function `inputGetChar()`.
+
+```plaintext
 getchar()
-    - Returns the value of next single character input.
+    - A function that returns the value of next single character input.
+```
+
+> - Refer to `code-04.c` function `inputGets()`.
+
+```plaintext
+gets()
+    - A function that reads the input as an ordered sequence of
+      characters, also called as "string".
+
+    string
+        - Stored in char array.
 ```
--- 01-sololearn-introduction-to-C/01-basic-concepts/code-04.c
+++ 01-sololearn-introduction-to-C/01-basic-concepts/code-04.c
@@ -4,6 +4,7 @@ void newLine(void) { printf("\n"); }
 
 void inputGetChar(void) {
   char letter;
+  char garbage[100];
 
   printf("GETCHAR FUNCTION EXAMPLE");
   newLine();
@@ -14,12 +15,39 @@ void inputGetChar(void) {
 
   // Output the input.
   printf("Output: %c", letter);
+
+  // Get char can only handle 1 character.
+  // The remaining characters will still remain in the input buffer.
+  // This handles the remaining characters.
+  // Acting as a garbage collector.
+  fgets(garbage, 100, stdin);
+
+  newLine();
+}
+
+void inputGets(void) {
+  char planet[10];
+
+  printf("GETS FUNCTION EXAMPLE");
+  newLine();
+
+  // Ask the input.
+  printf("Input: ");
+  fgets(planet, 10, stdin);
+
+  // Output the input.
+  printf("Output: %s", planet);
   newLine();
 }
 
 int main(void) {
-  // Example for getChar function.
+  // Example for getChar() function.
   inputGetChar();
+  newLine();
+
+  // Example for gets() function.
+  inputGets();
+  newLine();
 
   return 0;
 }
