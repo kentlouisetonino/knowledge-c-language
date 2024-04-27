2ec71f84bd8106cf05806ae6a9543fc726779b7c
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-27 19:14:19 +0800

    Code boilerplate.

---
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/code-04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void clearTerminal(void) { system("clear"); }
+
+void newLine(void) { printf("\n"); }
+
+void newTab(void) { printf("\t"); }
+
+int main(void) { return 0; }
