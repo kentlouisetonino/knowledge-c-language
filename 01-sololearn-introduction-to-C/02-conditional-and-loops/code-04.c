cfccf67f57c2ab812bff6504927d2d6c2a1949e2
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-27 22:34:36 +0800

    Logical and operator example.

--- 01-sololearn-introduction-to-C/02-conditional-and-loops/code-04.c
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/code-04.c
@@ -7,4 +7,45 @@ void newLine(void) { printf("\n"); }
 
 void newTab(void) { printf("\t"); }
 
-int main(void) { return 0; }
+void andOperator(void) {
+  int num1;
+  int num2;
+  char *result;
+
+  // Description of the function.
+  newTab();
+  printf("LOGICAL && OPERATOR EXAMPLE");
+  newLine();
+
+  // Ask the inputs.
+  newTab();
+  printf("First Number: ");
+  scanf("%d", &num1);
+  newTab();
+  printf("Second Number: ");
+  scanf("%d", &num2);
+
+  // Check if both numbers are divisible by 2.
+  if (((num1 % 2) == 0) && ((num2 % 2) == 0)) {
+    result = "true";
+  } else {
+    result = "false";
+  }
+
+  // Output the result.
+  newTab();
+  printf("Are both divisible by 2?: %s", result);
+  newLine();
+}
+
+int main(void) {
+  // Clean up the terminal first..
+  clearTerminal();
+  newLine();
+
+  // Logical && operator example.
+  andOperator();
+  newLine();
+
+  return 0;
+}
