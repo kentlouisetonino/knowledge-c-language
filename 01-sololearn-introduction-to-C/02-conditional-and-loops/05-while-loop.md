5488b39f982ff1decf09307cac98f2d6568544fc
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-28 13:04:09 +0800

    Sample while loop.

---
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/05-while-loop.md
@@ -0,0 +1,12 @@
+### The While Loop
+#
+
+> - The `while` statement is called a `loop structure` because it executes
+    statements repeatedly while an expression is true, looping over and
+    over again.
+
+```c
+while (expression) {
+    statements;
+}
+```
--- 01-sololearn-introduction-to-C/02-conditional-and-loops/code-05.c
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/code-05.c
@@ -7,9 +7,36 @@ void newLine(void) { printf("\n"); }
 
 void newTab(void) { printf("\t"); }
 
+void whileLoop(void) {
+  int count = 1;
+  int maximumLoopIteration;
+
+  // Function description.
+  newTab();
+  printf("WHILE LOOP EXAMPLE");
+  newLine();
+
+  // Ask the input.
+  newTab();
+  printf("Enter a maximum loop iteration: ");
+  scanf("%d", &maximumLoopIteration);
+
+  // Display the output.
+  while (count <= maximumLoopIteration) {
+    newTab();
+    printf("Count: %d", count);
+    newLine();
+    count++;
+  }
+}
+
 int main(void) {
   // Cleanup the terminal.
   clearTerminal();
+  newLine();
+
+  // While loop example.
+  whileLoop();
   newLine();
 
   return 0;
