7300642529b18e44adf35bdd6b22e6eefeea85c4
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-01 20:50:14 +0800

    For loop example.

--- 01-sololearn-introduction-to-C/02-conditional-and-loops/06-for-loop.md
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/06-for-loop.md
@@ -1,4 +1,21 @@
 ### The For Loop
 #
 
-> -
+> - The `for` statement is a loop that executes statements a fixed
+    number of times.
+
+```c
+for (initValue; condition; increment) {
+    statements;
+}
+```
+
+> - The `initValue` is a counter set to an initial value and
+    performed only once.
+
+> - The `condition` is a Boolean expression that compares the
+    counter to a value before each loop iteration, stopping the
+    loop when false is returned.
+
+> - The `increment` increases (or decreases) the counter by a set
+    value.
--- 01-sololearn-introduction-to-C/02-conditional-and-loops/code-06.c
+++ 01-sololearn-introduction-to-C/02-conditional-and-loops/code-06.c
@@ -7,9 +7,36 @@ void newLine(void) { printf("\n"); }
 
 void newTab(void) { printf("\t"); }
 
+void forLoop(void) {
+  int count;
+  int maximumCount;
+
+  // Function description.
+  newTab();
+  printf("FOR LOOP EXAMPLE");
+  newLine();
+
+  // Ask for the input.
+  newTab();
+  printf("Enter a maximum number: ");
+  scanf("%d", &maximumCount);
+
+  // Loop through and display the count.
+  // Count incrementally.
+  for (count = 1; count <= maximumCount; count++) {
+    newTab();
+    printf("Count: %d", count);
+    newLine();
+  }
+}
+
 int main(void) {
   // Clean the terminal.
   clearTerminal();
+  newLine();
+
+  // For loop example.
+  forLoop();
   newLine();
 
   return 0;
