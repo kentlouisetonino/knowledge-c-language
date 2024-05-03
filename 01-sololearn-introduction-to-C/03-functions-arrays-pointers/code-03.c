12129a64dd8f41553b1bbd6530b6889b6631d4e9
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-03 21:48:44 +0800

    Code 03 templace.

--- 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-03.c
+++ 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-03.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void new_line(void) { printf("\n"); }
+
+void new_tab(void) { printf("\t"); }
+
+void clear_terminal(void) { system("clear"); }
+
+int main(void) {
+  // Cleanup terminal.
+  clear_terminal();
+  new_line();
+
+  return 0;
+}
