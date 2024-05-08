3a8209f6805c0d00f5e203e38ffbdad70e761d18
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-08 22:23:07 +0800

    Looping through two-dimensinal array example.

--- 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-04.c
+++ 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-04.c
@@ -12,6 +12,7 @@ void two_dimensional_array(void) {
   a[0][0] = 1;
   a[0][1] = 2;
 
+  // Description and output.
   new_tab();
   printf("TWO-DIMENSIONAL ARRAY EXAMPLE");
   new_line();
@@ -23,6 +24,27 @@ void two_dimensional_array(void) {
   new_line();
 }
 
+void loop_two_dimensional_array(void) {
+  int numbers[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  int sum = 0;
+
+  // Description.
+  new_tab();
+  printf("LOOPING THROUGH TWO-DIMENSIONAL ARRAY EXAMPLE");
+  new_line();
+
+  // Compute the sum.
+  for (int row = 0; row < 2; row++) {
+    for (int column = 0; column < 3; column++) {
+      sum += numbers[row][column];
+    }
+  }
+
+  new_tab();
+  printf("Sum: %d", sum);
+  new_line();
+}
+
 int main(void) {
   // Cleanup the terminal.
   clear_terminal();
@@ -30,6 +52,10 @@ int main(void) {
 
   // Two-dimensional array example.
   two_dimensional_array();
+  new_line();
+
+  // Looping through two-dimensional array example.
+  loop_two_dimensional_array();
   new_line();
 
   return 0;
