289c2f27da79241dd7f7c08afdee496082629253
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-11 23:06:33 +0800

    Code 05.

--- 01-sololearn-introduction-to-C/03-functions-arrays-pointers/05-pointers.md
+++ 01-sololearn-introduction-to-C/03-functions-arrays-pointers/05-pointers.md
@@ -3,3 +3,8 @@
 
 > - The `scanf` function places the value at the location, or
     `address`, of the variable.
+
+> - A memory address is given as a hexadecimal number.
+
+> - The address of a variable stays the same from the time it
+    is declared until the end of its scope.
--- 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-05.c
+++ 01-sololearn-introduction-to-C/03-functions-arrays-pointers/code-05.c
@@ -7,9 +7,48 @@ void add_new_line(void) { printf("\n"); }
 
 void add_new_tab(void) { printf("\t"); }
 
+void variable_address(void) {
+  int num;
+  int *addressOfX;
+
+  // App description.
+  add_new_tab();
+  printf("VARIABLE ADDRESS EXAMPLE");
+  add_new_line();
+
+  // Ask the input.
+  add_new_tab();
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  // Display the input value.
+  add_new_tab();
+  printf("Value: %d", num);
+  add_new_line();
+
+  // Display the hex value.
+  add_new_tab();
+  printf("Hex Value: %x", num);
+  add_new_line();
+
+  // Diplay the pointer value using p format specifier.
+  add_new_tab();
+  addressOfX = &num;
+  printf("Pointer Value: %p", addressOfX);
+  add_new_line();
+
+  add_new_tab();
+  printf("Pointer Value: %d", num >> 1);
+  add_new_line();
+}
+
 int main(void) {
   // Clear the terminal.
   clear_terminal();
+  add_new_line();
+
+  // Variable address example.
+  variable_address();
   add_new_line();
 
   return 0;
