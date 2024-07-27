28b68d99124e0dccf048f63dfab3513eec325cae
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-07-27 23:22:17 +0800

    Changes.

--- tinkering-input/double_input.c
+++ tinkering-input/double_input.c
@@ -8,7 +8,15 @@ int main(void) {
   // If input is not numbers, it will return 0.000000.
   printf("Enter input: ");
   scanf("%lf", &input);
-  printf("%lf", input);
+
+  // Display the output.
+  printf("Double Input: %lf", input);
+  add_new_line();
+  printf("Equal to 0: %d", input == 0.000000);
+  add_new_line();
+  printf("Less than 0: %d", input < (double)0);
+  add_new_line();
+  printf("Greater than 0: %d", input > (double)0);
 
   add_new_line();
   add_new_line();
